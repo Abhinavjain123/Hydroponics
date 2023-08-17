5c1674ede08d989aa394c3608e5195a19f2b1c71
Puneet-1003 <puneetagarwal867@gmail.com>
2023-08-17 14:13:21 +0530

    Add second Activity change first activity only

--- app/src/main/java/com/example/hydroponics/SecondActivity.java
+++ app/src/main/java/com/example/hydroponics/SecondActivity.java
@@ -3,16 +3,12 @@ package com.example.hydroponics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
-    //Button view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //view=findViewById(R.id.button);
-
+        setContentView(R.layout.activity_second);
     }
 }
